4713babe07d9849668b866128f998d0df5685087
Michel Boudreau <michelb@pageuppeople.com>
2015-09-11 16:03:16 +1000

    updating pact-node to latest, fixing some of the tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,40 +1,45 @@
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  grunt.initConfig({
-    clean: {
-    },
+	grunt.initConfig({
+		clean: {},
 
-    pact: {
-      default: {
-      },
-      withOptions: {
-        options: {
-          port:9000
-        }
-      }
-    },
+		pact: {
+			default: {},
+			withOptions: {
+				options: {
+					port: 9000,
+					host: 'localhost',
+					/* dir: require('path').resolve(__dirname, '../.tmp' + Math.floor(Math.random() * 1000)),*/
+					cors: true,
+					log: 'log.txt',
+					spec: 1,
+					consumer: 'consumerName',
+					provider: 'providerName'
+				}
+			}
+		},
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js']
-    }
+		// Unit tests.
+		nodeunit: {
+			tests: ['test/**/*.spec.js']
+		}
 
-  });
+	});
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+	// Actually load this plugin's task(s).
+	grunt.loadTasks('tasks');
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['pact', 'nodeunit', 'pact:default:stop', 'pact:withOptions:stop']);
+	// Whenever the "test" task is run, first clean the "tmp" dir, then run this
+	// plugin's task(s), then test the result.
+	grunt.registerTask('test', ['pact', 'nodeunit', 'pact::stop']);
 
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['test']);
+	// By default, lint and run all tests.
+	grunt.registerTask('default', ['test']);
 
 };
