c060a5a896eead2393556f714590036cce7173c9
Michel Boudreau <michelboudreau@gmail.com>
2016-05-18 01:23:25 +1000

    removing grunt-wait task loading

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,7 +57,6 @@ module.exports = function (grunt) {
 	// These plugins provide necessary tasks.
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-nodeunit');
-	grunt.loadNpmTasks('grunt-wait');
 	
 	// Whenever the "test" task is run, first clean the "tmp" dir, then run this
 	// plugin's task(s), then test the result.
